3fc22bfce2338142faa3f585ac63eb2bc007edf4
Juan Cazala <cazala@mail.sfsu.edu>
2016-08-15 20:44:07 -0300

    React

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   resolve: {
+    alias: {
+      'components': 'src/components'
+    },
     extensions: ['', '.js', '.json', '.css', 'scss'],
     modulesDirectories: ['.', 'src', 'node_modules']
   },
